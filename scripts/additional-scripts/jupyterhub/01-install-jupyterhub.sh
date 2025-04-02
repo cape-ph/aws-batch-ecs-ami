47f9e37186af74d1f062156ce7099385169b5bd1
Micah Halter <micah.halter@gtri.gatech.edu>
2025-04-02 11:56:05 -0400

    feat(jupyterhub): add default configuration and `systemd` service

--- scripts/additional-scripts/jupyterhub/01-install-jupyterhub.sh
+++ scripts/additional-scripts/jupyterhub/01-install-jupyterhub.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 set -ex
 
+AMI_SCRIPTS=/tmp/additional-scripts/"${AMI_PREFIX}"
+JUPYTERHUB_PATH=/opt/jupyterhub
+
 # install http proxy
 sudo npm install -g configurable-http-proxy
 
 # create jupyterhub environment
-sudo python3 -m venv /opt/jupyterhub_env
+sudo python3 -m venv "${JUPYTERHUB_PATH}"
 
 # install jupyterhub into our environment
-sudo /opt/jupyterhub_env/bin/pip install jupyterhub jupyterlab notebook
+sudo "${JUPYTERHUB_PATH}"/bin/pip install jupyterhub jupyterlab notebook
+
+# copy default jupyterhub config
+sudo mkdir -p "${JUPYTERHUB_PATH}"/etc/jupyterhub
+sudo cp "${AMI_SCRIPTS}"/jupyterhub_config.py "${JUPYTERHUB_PATH}"/etc/jupyterhub/jupyterhub_config.py
+
+# copy jupyterhub service file
+sudo mkdir -p "${JUPYTERHUB_PATH}"/etc/systemd
+sudo cp "${AMI_SCRIPTS}"/jupyterhub.service "${JUPYTERHUB_PATH}"/etc/systemd/jupyterhub.service
+sudo ln -s "${JUPYTERHUB_PATH}"/etc/systemd/jupyterhub.service /etc/systemd/system/jupyterhub.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now jupyterhub.service
